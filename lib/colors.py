34ca5b657906246542a605b723b2502b2e0a43c0
YuvalAvra <yuval.avrahami@gmail.com>
2019-08-17 11:08:18 -0700

    lib/colors.py comments

--- lib/colors.py
+++ lib/colors.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-VALID_ATTRIBUTES = {    
-    "HEADER" : '\001\033[95m\002',
-    "BLUE" :'\001\033[94m\002',
-    "GREEN" : '\001\033[92m\002',
-    "WARNING" : '\001\033[93m\002',
-    "FAIL" : '\001\033[91m\002',
-    "BOLD" : '\001\033[1m\002',
-    "UNDERLINE" : '\001\033[4m\002'
-}
-
-ENDC = '\001\033[0m\002' # signals attributes reset
-"""
-
-
 VALID_ATTRIBUTES = {    
     "HEADER" : '\033[95m',
     "BLUE" :'\033[94m',
@@ -25,13 +10,13 @@ VALID_ATTRIBUTES = {
     "UNDERLINE" : '\033[4m'
 }
 
-ENDC = '\033[0m' # signals attributes reset
+ENDC = '\033[0m'  # signals attributes reset
+
+START_NON_PRINT_IGNORE = "\001"  # start of non-printable sequence
+END_NON_PRINT_IGNORE = "\002"    # end of non-printable sequence
 
 
-START_NON_PRINT_IGNORE = "\001"
-END_NON_PRINT_IGNORE = "\002"
-
-# Formats a text according to given attributes
+# Formats a text according to the given attributes
 def colored(text, attributes):
 	formated = text
 	for attribute in attributes:
@@ -41,7 +26,8 @@ def colored(text, attributes):
 			raise KeyError("No such attribute '{}'".format(attribute))
 	return formated + ENDC
 
-
+# Formats a text according to the given attributes
+# Wraps ANSI codes with non-printable delimiters 
 def colored_for_input(text, attributes):
 	formated = text
 	for attribute in attributes:
@@ -56,9 +42,25 @@ def colored_for_input(text, attributes):
 """
 For testing only
 """
-
 if __name__ == "__main__":
 	print("Testing mode (:")
 	head = colored("zzz", ["HEADER"])
 	print(list(head))
 	print(head)
+
+
+"""
+# Old way
+
+VALID_ATTRIBUTES = {    
+    "HEADER" : '\001\033[95m\002',
+    "BLUE" :'\001\033[94m\002',
+    "GREEN" : '\001\033[92m\002',
+    "WARNING" : '\001\033[93m\002',
+    "FAIL" : '\001\033[91m\002',
+    "BOLD" : '\001\033[1m\002',
+    "UNDERLINE" : '\001\033[4m\002'
+}
+
+ENDC = '\001\033[0m\002' # signals attributes reset
+"""
